841b0969799d916eb5044b664759dbebc284c6a4
ERaufi <edrisraufi@gmail.com>
2025-06-04 01:31:55 +0300

    Implement StudentsController with route handling for student data

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class StudentsController extends Controller
+{
+    //
+    public function index()
+    {
+        return inertia('Students/Index');
+        return Inertia::render('Students/Index');
+    }
+
+    public function withData()
+    {
+        return inertia('Students/Index', [
+            'abc' => 'bbc',
+            'dd' => 'dd'
+        ]);
+    }
+
+    public function withRouteParameters($name, $last_name)
+    {
+        return Inertia::render('Students/Index', [
+            'abc' => $name,
+            'dd' => $last_name,
+        ]);
+    }
+
+    public function withOptionalRouteParameters($name = 'Guest', $last_name = 'User')
+    {
+        return Inertia::render('Students/Index', [
+            'abc' => $name,
+            'dd' => $last_name,
+        ]);
+    }
+}
